17aabf91cf314be282f05db3ca346af061ea9417
AcostaDDev <acostadavdevelopment>
2024-04-14 19:28:49 +0200

    Creados GET POST PUT y DELETE con herramientas de django

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from .views import TaskView
 
 urlpatterns = [
     path('tasks/', TaskView.as_view(), name='task_list'),
+    path('tasks/<int:pk>', TaskView.as_view(), name='task_detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -8,22 +8,33 @@ from .models import Task
 
 class TaskView(View):
 
-    @csrf_exempt
+    @csrf_exempt                                            # Ignoramos la validacion de CSRF para que nos deje realizar la peticion POST
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
 
 
-    def get(self, request):
-        tasks = list(Task.objects.values())
-        if tasks:
-            datos = {'message': 'Hecho!', 'tasks': tasks}
+    def get(self, request, pk=0):
+
+        if pk > 0:
+            tasks = list(Task.objects.filter(id=pk).values())
+            if tasks:
+                task = tasks[0]
+                datos = {'message': f'Consulta con id {task['id']}', 'task': task}
+            else:
+                datos = {'message' : f'No hay tareas con el id {pk}'}
+
         else:
-            datos = {'message': 'No hay tareas'}
+
+            tasks = list(Task.objects.values())
+            if tasks:
+                datos = {'message': 'Hecho!', 'tasks': tasks}
+            else:
+                datos = {'message': 'No hay tareas'}
 
         return JsonResponse(datos)
     
     def post(self, request):
-        postr = json.loads(request.body)
+        postr = json.loads(request.body)        # Cargamos el cuerpo del json posteado --> Datos a insertar
         try: 
             task = Task.objects.create(
                 title = postr['title'],
@@ -35,8 +46,27 @@ class TaskView(View):
             datos = {'message': 'Error al insertar datos'}
         return JsonResponse(datos)
 
-    def put(self, request):
-        pass
-    def delete(self, request):
-        pass
+    def put(self, request, pk=0):
+        putr = json.loads(request.body)        # Cargamos el cuerpo del json posteado --> datos a insertar
+        tasks = list(Task.objects.filter(id=pk).values())
+
+        if tasks:
+            task = Task.objects.get(id=pk)      # Coge el objeto para poder modificarlo, no el valor del mismo a diferencia del método get
+            task.title = putr['title']
+            task.description = putr['description']
+            task.complete = putr['complete']
+            task.save()
+            datos = {'message': 'Datos actualizados'}
+        else:
+            datos = {'message' : f"No hay tareas con el id {pk}"}
+        return JsonResponse(datos)
+
+    def delete(self, request, pk=0):
+        tasks = list(Task.objects.filter(id=pk).values())
+        if tasks:
+            Task.objects.filter(id=pk).delete()
+            datos = {'message': 'Tarea eliminada'}
+        else:
+            datos = {'message' : f"No hay tareas con el id {pk}"}
+        return JsonResponse(datos)
 
