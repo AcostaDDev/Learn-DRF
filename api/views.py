0e275698d1f8b5937e8a9205259df3cb9cf5431d
AcostaDDev <acostadavdevelopment>
2024-04-14 20:34:50 +0200

    Autenticación sin token

--- api/views.py
+++ api/views.py
@@ -6,6 +6,9 @@ from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
 
 from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .models import Task
 from .serializers import TaskSerializer
@@ -84,3 +87,14 @@ class TaskViewXML(View):
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format = None):
+        content = {
+            'user': str(request.user), # django.contrib.auth.User 
+            'auth': str(request.auth), # None
+        }
+        return Response(content)
+
